e15255377a5b5fe28f0de7c9c9203fb4ab2d1555
supto <salatussadik131@gmail.com>
2022-09-03 12:28:02 +0600

    add all

--- index.js
+++ index.js
@@ -93,6 +93,32 @@ document.querySelector('#next-year').onclick = () => {
 //     document.querySelector('body').classList.toggle('dark')
 // }
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "500px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+// colaps start
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
 
 
+// colaps end
